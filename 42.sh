60bd973a79f2630e4f34a072ad3295d98e23401d
audegsper <audegsper@github.com>
2021-11-18 05:08:40 +0900

    done

--- 42.sh
+++ 42.sh
@@ -1,5 +1,5 @@
-# git add .
-# echo "git commit -m에 넣을 메세지를 넣어주세요."
-# read  msg
-# git commit -m "$msg"
-# git push "주소 넣기"
+git add .
+echo "git commit -m에 넣을 메세지를 넣어주세요."
+read  msg
+git commit -m "$msg"
+git push "주소 넣기"
